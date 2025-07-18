a112b5676ea1c3fd256d855506062c8fefeeed5d
Abayellow <sasha.abanshin@gmail.com>
2025-07-18 15:18:19 +1100

    Initial Commit

---
+++ Moonshot/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Moonshot
+//
+//  Created by Alexander Abanshin on 18.07.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Alexander Abanshin on 18.07.25.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
